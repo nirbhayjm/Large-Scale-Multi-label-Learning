764f071d7923e6622a5d45e957a375a98cef5eb8
Vikas Jain <vksjn18@gmail.com>
2017-06-25 07:00:54 +0530

    git problems

--- src/run_shell_scripts/run_synthdata.sh
+++
@@ -1,3 +0,0 @@
- python SD-exp1.py --dataset="../data/sdata.mat" -shuffle_minibatches -v -o -bs=500 -n_components=11 -lam_u=1e-3 -lam_v=1e-1 \
- -lam_w=1e-1 -lr_alpha=1.0 -lr_tau=0. -num_epochs=10 \
- -init_mu_a=1.0 -init_mu_b=1.0
